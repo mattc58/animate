5129828077788c131441c895ae2283b6b785666e
Matt Culbreth <mattc@freeze.local>
2010-01-24 13:53:46 -0500

    using with-command-line

--- src/animate.clj
+++ src/animate.clj
@@ -1,6 +1,14 @@
 (ns animate
-  (:gen-class))
+  (:gen-class)
+  (:use clojure.contrib.command-line))
  
 (defn -main [& args]
-  (println "Animate: bringing Clojure web applications to life"))
+    ;; the main function, gets called on startup to process command line args
+    (with-command-line args
+        "Animate: bringing Clojure web applications to life"
+        [[port "The port to use" 5858]
+         [ip "This is the IP address to use" "127.0.1.1"]
+         [config-dir "The directory to use for application config file" "."]
+         [tmp-dir "This is the tmp directory" "/tmp"]
+     remaining]))
   
