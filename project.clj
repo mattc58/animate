e53d3128008223cf758a1d607a85499cb1e4ddb8
Matt Culbreth <mattc@freeze.local>
2010-01-24 13:16:44 -0500

    integrated leinengen; first version of animate.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject animate "0.1"
+    :dependencies [[org.clojure/clojure
+                      "1.1.0-master-SNAPSHOT"]
+                   [org.clojure/clojure-contrib
+                      "1.0-SNAPSHOT"]]
+    :main animate)
+    
---
+++ src/animate.clj
@@ -0,0 +1,6 @@
+(ns animate
+  (:gen-class))
+ 
+(defn -main [& args]
+  (println "Animate: bringing Clojure web applications to life"))
+  
