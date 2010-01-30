559c28d5053adfe7137fa3f521feb6cc6d3d0fb6
mattc58 <mattculbreth@gmail.com>
2010-01-30 10:57:06 -0500

    just testing some stuff

--- src/animate.clj
+++ src/animate.clj
@@ -1,9 +1,10 @@
-(ns animate
+(ns src.animate
   (:gen-class)
   (:use clojure.contrib.command-line))
  
 (defn -main [& args]
     ;; the main function, gets called on startup to process command line args
+    (println "hello!")
     (with-command-line args
         "Animate: bringing Clojure web applications to life"
         [[port "The port to use" 5858]
@@ -11,4 +12,9 @@
          [config-dir "The directory to use for application config file" "."]
          [tmp-dir "This is the tmp directory" "/tmp"]
      remaining]))
+     
+(defn matt
+    []
+    (+ 2 5))
+     
   
