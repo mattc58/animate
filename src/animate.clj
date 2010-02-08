970fb04178cb1b6cb177ea7cf277ef29a8895f5b
Matt Culbreth <mattc@freeze.local>
2010-02-07 19:58:48 -0700

    working serve-resource, 404 also

--- src/animate.clj
+++ src/animate.clj
@@ -4,7 +4,8 @@
   (:gen-class)
   (:use clojure.contrib.command-line)
   (:use clojure.contrib.server-socket)
-  (:import (java.io BufferedReader InputStreamReader OutputStreamWriter)))
+  (:use clojure.contrib.duck-streams)
+  (:import (java.io File FileNotFoundException BufferedReader InputStreamReader OutputStreamWriter)))
  
 (defn- writeln
     " utility function to write a string and a newline to a stream "
@@ -44,11 +45,40 @@
                         (writeln client-out line))
                 (recur (cons input lines))))))))
 
-(defn serve-resource
-    [stream verb resource-path]
-    (println "Will send " resource-path))
+(defn make-success-header
+    " make the HTTP 200 header "
+    [content-length]
+    (list 
+        "HTTP/1.1 200 OK"
+        "Server: Animate"
+        "X-Powered-By: Animate"
+        "Content-Type: text/html; charset=utf-8"
+    (str "Content-Length: " content-length) "\n"))
     
-                    
+(defn make-404-header
+    " make the HTTP 404 not found header "
+    [content-length]
+    (list 
+        "HTTP/1.1 404 Not Found"
+        "Server: Animate"
+        "X-Powered-By: Animate"
+        "Content-Type: text/html; charset=utf-8"
+    (str "Content-Length: " content-length) "\n"))
+
+(defn serve-resource
+    " serve an actual resource (a file) "
+    [stream verb resource-path]
+    (let [file-name (str "." resource-path)
+        resource-file (File. file-name)]
+        (if
+            (.exists resource-file)
+            (let [resource (slurp file-name)]
+                (doseq [line (concat (make-success-header (.length resource)) (list resource))]
+                    (writeln stream line)))
+            (let [notfound (slurp "404.html")]
+                (doseq [line (concat (make-404-header (.length notfound)) (list notfound))]
+                    (writeln stream line))))))
+
 (defn handle-request
     " the function that handles the client request "
     [in out]
