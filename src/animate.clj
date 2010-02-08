2057e305fc2d5a0d017ec637a5f49a28395557e9
Matt Culbreth <mattc@freeze.local>
2010-02-08 07:45:32 -0700

    working with styles, jpg, html, 404

--- src/animate.clj
+++ src/animate.clj
@@ -5,6 +5,7 @@
   (:use clojure.contrib.command-line)
   (:use clojure.contrib.server-socket)
   (:use clojure.contrib.duck-streams)
+  (:use clojure.contrib.str-utils)
   (:import (java.io File FileNotFoundException BufferedReader InputStreamReader OutputStreamWriter)))
  
 (defn- writeln
@@ -49,37 +50,63 @@
 (defn make-css-header
     " make a CSS header "
     [content-length]
-    (list
-        "HTTP/1.1 200 OK"
-        "Content-Type: text/css"
-        (str "Content-Length: " content-length) 
-        "Server: Animate"
-        "X-Powered-By: Animate"
-        "\n"
-        ))
+    (str-join "\n"
+        (list
+            "HTTP/1.1 200 OK"
+            "Content-Type: text/css"
+            (str "Content-Length: " content-length) 
+            "Server: Animate"
+            "X-Powered-By: Animate"
+            "\n"
+            )))
         
+(defn make-image-header
+    " make a CSS header "
+    [content-length extension]
+    (str-join "\n"
+        (list
+            "HTTP/1.1 200 OK"
+            (str "Content-Type: image/" extension)
+            (str "Content-Length: " content-length) 
+            "Server: Animate"
+            "X-Powered-By: Animate"
+            "\n"
+            )))
+
 (defn make-success-header
     " make the HTTP 200 header "
     [content-length]
-    (list 
-        "HTTP/1.1 200 OK"
-        "Server: Animate"
-        "X-Powered-By: Animate"
-        "Content-Type: text/html; charset=utf-8"
-        (str "Content-Length: " content-length) 
-        "\n"))
+    (str-join "\n"
+        (list 
+            "HTTP/1.1 200 OK"
+            "Server: Animate"
+            "X-Powered-By: Animate"
+            "Content-Type: text/html; charset=utf-8"
+            (str "Content-Length: " content-length) 
+        "\n")))
     
 (defn make-404-header
     " make the HTTP 404 not found header "
     [content-length]
-    (list 
-        "HTTP/1.1 404 Not Found"
-        "Server: Animate"
-        "X-Powered-By: Animate"
-        "Content-Type: text/html; charset=utf-8"
-        (str "Content-Length: " content-length) 
-        "\n"))
+    (str-join "\n"
+        (list 
+            "HTTP/1.1 404 Not Found"
+            "Server: Animate"
+            "X-Powered-By: Animate"
+            "Content-Type: text/html; charset=utf-8"
+            (str "Content-Length: " content-length) 
+            "\n")))
 
+(defn write-resource
+    " write a resourse with its header and then its content "
+    [stream header content]
+    (doto stream
+        (.write header)
+        (.flush)
+        (.write content)
+        (.flush)
+    ))
+    
 (defn serve-resource
     " serve an actual resource (a file) "
     [stream verb resource-path]
@@ -89,16 +116,16 @@
         (if
             (.exists resource-file)
             (let [resource (slurp file-name)]
-                (doseq [line (concat 
-                    (if
-                        (.contains file-name ".css")
-                        (make-css-header (.length resource))
-                        (make-success-header (.length resource))
-                    )  (list resource))]
-                    (writeln stream line)))
+                (write-resource
+                    stream
+                    (cond
+                        (.contains file-name ".css") (make-css-header (.length resource))
+                        (.contains file-name ".jpg") (make-image-header (.length resource) "jpeg")
+                        :else (make-success-header (.length resource))
+                    ) 
+                    resource))
             (let [notfound (slurp "404.html")]
-                (doseq [line (concat (make-404-header (.length notfound)) (list notfound))]
-                    (writeln stream line))))))
+                (write-resource stream (make-404-header (.length notfound)) notfound)))))
 
 (defn handle-request
     " the function that handles the client request "
@@ -125,7 +152,6 @@
     " The main server process "
     [port]
     (println "Listening to port" port "...")
-    ;(create-server port echo-batch))
     (create-server port handle-request))
   
 (defn -main [& args]
