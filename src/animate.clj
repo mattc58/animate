e301fc0ed6b30b8bcfcbbb9c4a32278aa05cd47c
Matt Culbreth <mattc@freeze.local>
2010-02-07 17:05:14 -0700

    using an explicit stream in echo instead of the binded out

--- src/animate.clj
+++ src/animate.clj
@@ -4,21 +4,27 @@
   (:use clojure.contrib.server-socket)
   (:import (java.io BufferedReader InputStreamReader OutputStreamWriter)))
  
-;; Simple method to echo back to the client
+;; Simple function to echo back to the client
 (defn echo
     [in out]
-    (binding [*in* (BufferedReader. (InputStreamReader. in))
-        *out* (OutputStreamWriter. out)]
+    (binding [*in* (BufferedReader. (InputStreamReader. in))]
+        ;*out* (OutputStreamWriter. out)]
+    (let [client-out (OutputStreamWriter. out)]
+        (println "New client connection...")
         (loop []
             (let [input (read-line)]
                 (if 
                     ; this seems like a HACK.  Thought input would be nil but it's not.
+                    ; the important thing is that the function will exit if it's an
+                    ; empty line from the client, and the server-socket library
+                    ; will close the socket
                     (> (.length input) 0)
                     (do
-                        (println input)
-                        (flush)
+                        (.write client-out input)
+                        (.write client-out "\n")
+                        (.flush client-out)
                         (recur)
-                    ))))))
+                    )))))))
                           
 ;; The main server process 
 (defn run-server
