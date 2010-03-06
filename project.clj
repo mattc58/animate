e1882bcd71db0d50c807b2465471f6e024624b6c
Matt Culbreth <mattc@freeze.local>
2010-03-06 06:07:11 -0500

    put correct animate.core in project file

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
                       "1.2.0-master-SNAPSHOT"]
                    [org.clojure/clojure-contrib
                       "1.2.0-SNAPSHOT"]]
-    :main animate)
+    :main animate.core)
     
