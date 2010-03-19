a09de0b474ef77116a090b7d1f055ac39c2215b6
Matt Culbreth <mattc@freeze.local>
2010-03-19 12:07:12 -0400

    added the test file

---
+++ test/test_server.clj
@@ -0,0 +1,17 @@
+;; Tests of the server functionality
+
+(ns test-server
+    (:use [clojure.test :only (deftest, is, use-fixtures)])
+    (:use [clojure.contrib.server-socket])
+    (:use animate.core))
+    
+(def *animate-test-dir* "./animate-test")
+
+(defn server-fixture
+    [f]
+    (println "In server-fixture for " f)
+    (-main "--port" "6000")
+    (f))
+ 
+(use-fixtures :once server-fixture)
+
