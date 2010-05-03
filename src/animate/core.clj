01c1c196eca14fc954e5739f127e816e04d82057
mattc58 <mattculbreth@gmail.com>
2010-05-03 16:28:02 -0400

    it's working

--- src/animate/core.clj
+++ src/animate/core.clj
@@ -100,21 +100,29 @@
         (= type "404") (make-404-header content-length)
         :else (make-500-header content-length))))
     
+(defn write-resource
+    " write the resource "
+    [out header resource file]
+    (copy header out)
+    (if resource
+        (copy resource out)
+        (copy file out)))
+
 (defn serve-404
     " serve the 404 page for a site or the general one "
     [site-404-path stream]
     (try
         (let [notfound (if (nil? site-404-path) (slurp (str *config-dir* "/404.html")) (slurp site-404-path))]
-            (write-resource stream (make-header (.length notfound) nil) notfound))
+            (write-resource stream (make-header (.length notfound) nil) notfound nil))
     (catch FileNotFoundException e
         ;; can't find the 404 file (ironically), so try the general one
         (try
             (let [notfound (slurp (str *config-dir* "/404.html"))]
-                (write-resource stream (make-header (.length notfound) nil) notfound))
+                (write-resource stream (make-header (.length notfound) nil) notfound nil))
         (catch FileNotFoundException e
             ;; no site-wide 404, so just send a message
             (let [message "HTTP 404: Not Found\n"]
-                (write-resource stream (make-header (.length message) nil) message)))))))
+                (write-resource stream (make-header (.length message) nil) message nil)))))))
 
 (defn find-config
     " find the config for a given host-name "
@@ -122,12 +130,6 @@
     (filter #(not (nil? %)) 
         (map (fn [item] (if (not-empty (filter #(.startsWith host-name %) (:host-names item))) item nil)) configs)))
         
-(defn write-resource
-    " write the resource "
-    [out header file]
-    (copy header out)
-    (copy file out))
-
 (defn serve-resource
     " serve an actual resource (a file) "
     [stream configs http-request resource-path]
@@ -140,7 +142,7 @@
                  resource-file (File. file-name)]
                  (if
                      (.exists resource-file)
-                     (write-resource stream (make-header (.length resource-file) file-name) resource-file)
+                     (write-resource stream (make-header (.length resource-file) file-name) nil resource-file)
                      (serve-404 (str (:files-root (first host)) "/404.html") stream))))))
 
 (defn make-http-request
