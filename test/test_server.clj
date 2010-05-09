4a7387fc813a81718b99a836e5c8cc3cd7cb288e
mattc58 <mattculbreth@gmail.com>
2010-05-09 18:37:37 -0400

    got a test for if the server is alive

--- test/test_server.clj
+++ test/test_server.clj
@@ -1,17 +1,20 @@
 ;;; Tests of the server functionality
 ;
-;(ns test-server
-;    (:use [clojure.test :only (deftest, is, use-fixtures)])
-;    (:use [clojure.contrib.server-socket])
-;    (:use animate.core))
-;    
-;(def *animate-test-dir* "./animate-test")
-;
-;(defn server-fixture
-;    [f]
-;    (println "In server-fixture for " f)
-;    (-main "--port" "6000")
-;    (f))
-; 
-;(use-fixtures :once server-fixture)
-;
+(ns test-server
+   (:use [clojure.test :only (deftest, is, use-fixtures)])
+   (:use [clojure.contrib.server-socket])
+   (:use [clojure.contrib.io :only (reader, input-stream, read-lines)])
+   (:use animate.core))
+   
+(def *animate-test-dir* "./animate-test")
+
+(defn server-fixture
+   [f]
+   (-main "--port" "6000")
+   (f))
+
+(use-fixtures :once server-fixture)
+
+(deftest test-server-alive
+    (let [connection (input-stream "http://localhost:6000")]
+        (is (= 16 (count (read-lines (reader connection)))))))
