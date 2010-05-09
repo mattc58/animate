ac63a21314bbcbad2902725bcd601f5dc7b311e5
mattc58 <mattculbreth@gmail.com>
2010-05-09 18:55:51 -0400

    working server tests

--- test/test_server.clj
+++ test/test_server.clj
@@ -1,20 +1,45 @@
 ;;; Tests of the server functionality
+;; NOTE: the /etc/hosts file must have entries for any URLs tested here
 ;
 (ns test-server
-   (:use [clojure.test :only (deftest, is, use-fixtures)])
-   (:use [clojure.contrib.server-socket])
-   (:use [clojure.contrib.io :only (reader, input-stream, read-lines)])
-   (:use animate.core))
+    (:use [clojure.test :only (deftest, is, use-fixtures)])
+    (:use [clojure.contrib.server-socket])
+    (:use [clojure.contrib.io :only (reader, input-stream, read-lines)])
+    (:import (java.net UnknownHostException))
+    (:use animate.core))
    
 (def *animate-test-dir* "./animate-test")
 
 (defn server-fixture
-   [f]
-   (-main "--port" "6000")
-   (f))
+    " this server fixture will be run once and will start the test server "
+    [f]
+    (-main "--port" "6000")
+    (f)
+    (close-server animate-server))
 
 (use-fixtures :once server-fixture)
 
 (deftest test-server-alive
+    " test that the server is alive "
     (let [connection (input-stream "http://localhost:6000")]
-        (is (= 16 (count (read-lines (reader connection)))))))
+        (is (< 0 (count (read-lines (reader connection)))))))
+        
+(deftest test-mc-alive
+    " test that dev1.mattculbreth.com comes up "
+    (let [connection (input-stream "http://dev1.mattculbreth.com:6000")]
+        (is (< 0 (count (read-lines (reader connection)))))))
+
+(deftest test-no-mc-alive
+    " test that no dev3.mattculbreth.com comes up "
+    (is (thrown? UnknownHostException (def connection (input-stream "http://dev3.mattculbreth.com:6000")))))
+    
+(deftest test-yi-alive
+    " test that local.yieldidea.com comes up "
+    (let [connection (input-stream "http://local.yieldidea.com:6000")]
+        (is (< 0 (count (read-lines (reader connection)))))))
+
+(deftest test-mc-content
+    " test that the right content comes up for dev1.mattculbreth.com ")
+    
+(deftest test-yi-content
+    " test that the right content comes up for local.yieldidea.com ")
