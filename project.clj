bcb88e2ae4ef8a485dba958225c7c26cf272dd07
Matt Culbreth <mattc@hemispheres.(none)>
2010-07-23 16:03:39 -0400

    new settings for latest clojure

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject animate "0.1"
-    :dependencies [[org.clojure/clojure
-                      "1.2.0-master-SNAPSHOT"]
-                   [org.clojure/clojure-contrib
-                      "1.2.0-SNAPSHOT"]]
+(defproject animate "1.0"
+    :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                   [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
     :main animate.core)
     
