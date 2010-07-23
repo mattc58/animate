79cbf5b6ad263e72f3cea245f6a648182bde1854
Matt Culbreth <mattc@hemispheres.(none)>
2010-07-23 16:41:24 -0400

    incremented version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject animate "1.0"
+(defproject animate "1.1"
     :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                    [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
     :main animate.core)
