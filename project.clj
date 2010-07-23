e73ade5d15554ce631c93c3888e6e3290fac6a26
Matt Culbreth <mattc@hemispheres.(none)>
2010-07-23 16:59:00 -0400

    added lein as a dependency for cookies and session

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject animate "1.1"
     :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                   [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+                   [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                   [ring/ring-core "0.2.5"]]
     :main animate.core)
     
